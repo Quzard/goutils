9c635df5bc3d8bd624914298f32f02a5dc90145b
quzard <1191890118@qq.com>
2022-02-15 14:42:47 +0800

    时间轮

--- timewheel/timewheel_test.go
+++ timewheel/timewheel_test.go
@@ -1,4 +1,4 @@
-package timewheelcircle
+package timewheel
 
 import (
 	"fmt"
@@ -16,7 +16,7 @@ func TaskJob(key interface{}) {
 
 func TestName(t *testing.T) {
 	//初始化一个时间间隔是1s，一共有60个齿轮的时间轮盘，默认轮盘转动一圈的时间是60s
-	tw := CreateTimeWheel(1*time.Second, 60, TimeWheelDefaultJob)
+	tw := CreateTimeWheel(1*time.Second, 5, TimeWheelDefaultJob)
 
 	// 启动时间轮盘
 	tw.Start()
@@ -26,39 +26,38 @@ func TestName(t *testing.T) {
 		// 每隔10s执行一次
 		// task名字叫task1
 		// task的创建时间是time.Now()
-		// task执行的任务设置为nil，所以默认执行timewheel的Job，也就是TimeWheelDefaultJob
-		fmt.Println(fmt.Sprintf("%v Add task task-5s", time.Now().Format(time.RFC3339)))
-		err := tw.AddTask(5*time.Second, "task-5s", time.Now(), -1, nil)
+		// task执行的任务设置为nil，所以默认执行timewheel的Job，也就是example.TimeWheelDefaultJob
+		fmt.Println(fmt.Sprintf("%v Add task task1-5s", time.Now().Format(time.RFC3339)))
+		err := tw.AddTask(5*time.Second, "task1-5s", time.Now(), -1, nil)
 		if err != nil {
 			panic(err)
 		}
-
-		// 该Task执行TaskJob
-		fmt.Println(fmt.Sprintf("%v Add task task-2s", time.Now().Format(time.RFC3339)))
-		err = tw.AddTask(2*time.Second, "task-2s", time.Now(), -1, TaskJob)
+		// 该Task执行example.TaskJob
+		fmt.Println(fmt.Sprintf("%v Add task task2-5s", time.Now().Format(time.RFC3339)))
+		err = tw.AddTask(5*time.Second, "task2-5s", time.Now(), -1, TaskJob)
 		if err != nil {
 			panic(err)
 		}
 
 	} else {
-		panic("timewheel is not running")
+		panic("TimeWheel is not running")
 	}
-	time.Sleep(10 * time.Second)
+	time.Sleep(5 * time.Second)
 
 	// 删除task
-	fmt.Println("Remove task task-5s")
-	err := tw.RemoveTask("task-5s")
+	fmt.Println("Remove task task1-5s")
+	err := tw.RemoveTask("task1-5s")
 	if err != nil {
 		panic(err)
 	}
 
-	time.Sleep(10 * time.Second)
-
-	fmt.Println("Remove task task-2s")
-	err = tw.RemoveTask("task-2s")
+	// 删除task
+	fmt.Println("Remove task task2-5s")
+	err = tw.RemoveTask("task2-5s")
 	if err != nil {
 		panic(err)
 	}
+	time.Sleep(6 * time.Second)
 
 	// 关闭时间轮盘
 	tw.Stop()
